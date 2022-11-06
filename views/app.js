c42233501014b4425a17685b216512c9eb6ac02f
paolabrasca <pbrasca@gmail.com>
2022-11-05 22:25:49 -0300

    formulario

--- views/app.js
+++ views/app.js
@@ -2,46 +2,28 @@ let comprar = confirm("¿Deseas unirte al Mi Club?")
 
 let click = true;
 
-const fetchForm = (provincia) => {
-  return fetch ('https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre${provincia}')
-  .then (Response => Response.json())
-  .catch (error => console.log (error))
-}
+const aplicacion = document.querySelector ('.container1')
+const url = 'https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre'
 
-const $form = document.querySelector('form');
-const $input = document.querySelector('#input');
-const $submit = document.querySelector('#submit');
-const $results = document.querySelector('#results');
+fetch(url)
+.then(Response => Response.json())
+.then(function (provincias) {
+  provincias.forEach(function(provincia) {
+    const p = document.createElement('p');
+    p.innerHTML = provincia.nombre;
+    aplicacion.appendChild(p);
+  });
 
-$form.addEventListener('submit', async function(event){
-  event.preventDefault();
-  const {valor} = $input;
-
-  if(!valor)return
-
-  $submit.setAttribute('disabled', '');
-
-  let dataFetcthForm = await fetchForm (valor);
-
-  if (dataFetcthForm){
-  $results.innerHTML = JSON.stringify(dataFetcthForm, null,2);
-  }
-
-  $submit.removeAttribute('disabled'); 
-})
+})   
+.catch(err => console.log(err))
 
 
 
 
 if (click) {
   document.querySelector('p2').classList.add('nombreMiClub');
-}
+  }
 Swal.fire({
     title: '¡Genial!',
     text: 'Llena el siguiente formulario',
-    imageUrl: 'C:\Users\pc\Desktop\Codo a Codo\trabajo grupal\TRABAJO-PRACTICO-INNATO\views\imagenes\LOGO.png',
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  });
- 
+});
