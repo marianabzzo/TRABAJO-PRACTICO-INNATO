3796122e96857af14e7dac4d9b608ea4ecb2936f
paolabrasca <pbrasca@gmail.com>
2022-11-06 21:19:39 -0300

    formulario con provincias

--- views/app.js
+++ views/app.js
@@ -1,38 +1,69 @@
 let click = true;
 
-async function getProvincias(){
-let response  = await fetch('https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre')
-let data = await response.json()
+//async function getProvincias(){
+//let response  = await fetch('https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre')
+//let data = await response.json()
 
-console.log(data);
-}
+//console.log(data);
+//}
 
-getProvincias()
+//getProvincias()
 
-const $form = document.querySelector('form');
-const $input = document.querySelector('#input');
-const $submit = document.querySelector('#submit');
-const $results = document.querySelector('#results');
+//const $form = document.querySelector('form');
+//const $input = document.querySelector('#input');
+//const $submit = document.querySelector('#submit');
+//const $results = document.querySelector('#results');
 
-$form.addEventListener('submit', async function(event){
-  event.preventDefault();
-  const {valor} = $input;
+//$form.addEventListener('submit', async function(event){
+ // event.preventDefault();
+ // const {valor} = $input;
 
-  if(!valor)return
+  //if(!valor)return
 
-  $submit.setAttribute('disabled', '');
+  //$submit.setAttribute('disabled', '');
 
-  let dataFetcthForm = await fetchForm (valor);
+  //let dataFetcthForm = await fetchForm (valor);
 
-  if (dataFetcthForm){
-  $results.innerHTML = JSON.stringify(dataFetcthForm, null,2);
+  //if (dataFetcthForm){
+  //$results.innerHTML = JSON.stringify(dataFetcthForm, null,2);
+ // }
+//})
+
+
+//if (click) {
+ // document.querySelector('p2').classList.add('nombreMiClub');
+  //}
+
+
+  const selectMenu = document.querySelector('#provincias');
+  const apiGob = async () => {
+     await fetch('https://apis.datos.gob.ar/georef/api/provincias')
+      .then (res => res.json())
+      .then(data => { 
+      let output = '';
+      data.provincias.forEach(provincia => {
+      output += `<option>${provincia.nombre}</option>`;
+          console.log(provincia.nombre);
+      })
+      selectMenu.innerHTML = output;
+      // getInput();
+      
+      })
+      .catch(err => console.log(err))    
+  
+      rel();
   }
-})
-
-
-if (click) {
-  document.querySelector('p2').classList.add('nombreMiClub');
-  }
+  
+  apiGob();
+  
+  let rel = function selected(){
+      const selected = selectMenu.addEventListener('change', () =>{
+          let result = selectMenu.options[selectMenu.selectedIndex].value;
+          console.log(result)
+          return result
+      });
+      
+  }  
 Swal.fire({
     title: '¿Deseas unirte al Mi Club?' + '¡Genial!',
     text: 'Llena el siguiente formulario',
